aee8ce9bb560bf334e4eee526502c21cacaad751
pjwhite08 <pamelajeanwhite@gmail.com>
2021-10-17 18:44:34 -0500

    from August

--- src/dates/index.js
+++ src/dates/index.js
@@ -1,19 +1,34 @@
+console.log("I'm working")
+
+//moments docs https://momentjs.com/docs/#/displaying/
 // import moment here; use this package in each function
+const moment = require('moment')
+
+const day = moment().format('dddd')
+
+console.log('Today is', day)
 
 const today = () => {
-  // write code for dates.today
-
+  // write code for dates.today --> what does this mean? I have no idea what this means without the README
+  const day = moment().format('dddd')
+  return day
 }
+console.log(today())
 
 const calendar = () => {
   // write code for dates.calendar
-
+  let cal = moment().format(`${"MMM"} ${"D"}, ${"YYYY"}`)
+  // " " + "D" + ",""YYYY")
+  return cal
 }
+ console.log((calendar()))
 
 const currentTime = () => {
   // write code for dates.currentTime
-
+let time = moment().format(`:${"HH"}:${"mm"}:${"ss"} ${"A"}`)
+return time
 }
+console.log(currentTime())
 
 module.exports = {
   today,
--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,17 +1,44 @@
+console.log(`I'm working`)
+
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num%2 == 0){
+  return true
+    }
+  else {
+    return false
+    }
 }
+console.log(isEven(15))
 
+let arr = [4, 5, 6, 2]
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let runningSum = 0
+  for (i=0; i<arr.length; i++){
+    runningSum += arr[i]
+  }
+  return (runningSum)
 }
+console.log(sum(arr))
+
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  
+    // write code for numbers.sum
+    let runningSum = 0
+    for (i=0; i<arr.length; i++){
+      runningSum += arr[i]
+    }
+    if (runningSum = sum){
+      return true
+    }
+    else {
+      return false
+    }
 }
+console.log("comboSum. Does sum of array equal sum:", comboSum(arr, 22))
 
 module.exports = {
   isEven,
